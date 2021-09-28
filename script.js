2535970f96f8db9a88ed815b03c8d559832e80be
natikkr <82267830+natikkr@users.noreply.github.com>
2021-09-29 00:50:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,85 +5,83 @@ let postForm = document.forms.postData;
 let nameInput = postForm.elements.title;
 let authorInput = postForm.elements.author;
 
-
 button.addEventListener('click', function decorator(e) {
   e.preventDefault();
-  newPost={
-    "title" : nameInput.value,
-    "author" : authorInput.value
+  newPost = {
+    "title": nameInput.value,
+    "author": authorInput.value
   };
-  saveJSON('http://localhost:3000/posts', JSON.stringify(newPost)).then(function(data) {
+  saveJSON('http://localhost:3000/posts', JSON.stringify(newPost)).then(function (data) {
 
     let li = document.createElement('li');
     li.innerHTML = `title: ${newPost.title} of author:${newPost.author}`;
+
     ulCr.append(li);
     console.log('Success ', data);
     postForm.reset();
 
-  }, function(status) {
+  }, function (status) {
     console.log('wrong', status);
   });
 });
 
-let getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-      let xhr = new XMLHttpRequest();
+let getJSON = function (url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
 
-      xhr.open('get', url, true);
+    xhr.open('get', url, true);
 
-      xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-      xhr.onload = function() {
-          let status = xhr.status;
+    xhr.onload = function () {
+      let status = xhr.status;
 
-          if (status === 200) {
-              resolve(xhr.response);
-          } else {
-              reject(status);
-          }
-      };
-      xhr.send();
+      if (status === 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
   });
 };
 
-getJSON('http://localhost:3000/posts').then(function(data) {
-      console.log(data)
-      ulCr.innerHTML = data.map((post) => {
-        
-        let li = `<li>title: ${post.title} of author: ${post.author}</li>`;
-        console.log(li)
-        return li;
-      }).join('');
-      
-      posts.appendChild(ulCr);
-      
-      console.log('Success ', data);
-    }, function(status) {
-      console.log('wrong.', status);
-      
-    });
+getJSON('http://localhost:3000/posts').then(function (data) {
+  console.log(data)
+  ulCr.innerHTML = data.map((post) => {
+    let li = `<li>title: ${post.title} of author: ${post.author}</li>`;
+    console.log(li)
+    return li;
+  }).join('');
 
+  posts.appendChild(ulCr);
 
-  function saveJSON (url, data) {
-    console.log(nameInput.value + 'author ' + authorInput.value);
-    return new Promise(function(resolve, reject) {
-        let xhr = new XMLHttpRequest();
+  console.log('Success ', data);
+}, function (status) {
+  console.log('wrong.', status);
 
-        xhr.open('post', url, true);
-        xhr.setRequestHeader(
-            'Content-type', 'application/json; charset=utf-8'
-        );
+});
 
-        xhr.responseType = 'json';
+function saveJSON(url, data) {
+  console.log(nameInput.value + 'author ' + authorInput.value);
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
 
-        xhr.onload = function() {
-            let status = xhr.status;
+    xhr.open('post', url, true);
+    xhr.setRequestHeader(
+      'Content-type', 'application/json; charset=utf-8'
+    );
 
-            resolve(xhr.response);
-        };
-        xhr.onerror = function(e) {
-            reject("Error " + url);
-        };
-        xhr.send(data);
-    });
-  };
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+      let status = xhr.status;
+
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      reject("Error " + url);
+    };
+    xhr.send(data);
+  });
+};
